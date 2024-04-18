abf3bb4543e8381e06211c9938d156405246c08c
Kunal Gangani <150250846+kunal-gangani@users.noreply.github.com>
2024-04-18 19:08:26 +0530

    Added

--- Que1.dart
+++ Que1.dart
@@ -4,11 +4,13 @@ void main(){
   List myArray = array.toList();
   stdout.write("How many elements do you want in your Array : ");
   int len = int.parse(stdin.readLineSync()!);
-  for(int i = 1 ; i <= len ; i++){
-    stdout.write("Enter Element $i : ");
-    int el = int.parse(stdin.readLineSync()!);
-    myArray.add(el);
-  }
+  int i = 1;
+    while(i<=len){
+      stdout.write("Enter element $i value of Array : ");
+      int el = int.parse(stdin.readLineSync()!);
+      myArray.add(el);
+      i++;
+    }
   print("Elements in the Array are : ");
   myArray.forEach((element) {
       stdout.write("$element   ");
