bcf0cdb3343f08045498d964aa55a2ee27dd5f41
Kunal Gangani <150250846+kunal-gangani@users.noreply.github.com>
2024-04-23 15:12:36 +0530

    Update Que3.dart

--- Que3.dart
+++ Que3.dart
@@ -1,23 +1,34 @@
 import 'dart:io';
 void main() {
-  var array = [1, 2, 3, 4];
-  List myArray = array.toList();
+  List myArray = [];
+  print("Enter 3 elements first to Enter the Menu Driven Code : ");
+  myArray = List.generate(3, (i) {
+      stdout.write("Enter the value for Element ${i+1} : ");
+      int el = int.parse(stdin.readLineSync()!);
+      myArray.add(el);
+      return el;
+    });
   int choice;
   do {
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     print("Welcome to the Menu-Driven Program of Array Operations ");
     print("Enter 1 to Enter an element in the Array  ");
     print("Enter 2 to Update an element in the Array  ");
     print("Enter 3 to Remove an element in the Array  ");
     print("Enter 4 to View the elements in the Array  ");
     print("Enter 0 to Exit ");
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     stdout.write("Enter Your Choice : ");
     choice = int.parse(stdin.readLineSync()!);
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     switch (choice) {
       case 1:
         stdout.write("Enter the Element you want to add in your Array : ");
         int addVar = int.parse(stdin.readLineSync()!);
         myArray.add(addVar);
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         print(myArray);
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         break;
       case 2:
         stdout.write("Enter the Element Index where you want to Update : ");
@@ -25,28 +36,34 @@ void main() {
         print("");
         if(index>myArray.length){
           print("Index is Invalid!!");
+          print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         }
         else{
           stdout.write("Enter the Element you want : ");
           int newEle = int.parse(stdin.readLineSync()!);
           myArray[index] = newEle;
           print(myArray);
+          print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         }
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         break;
       case 3:
         stdout.write("Enter the Element you want to Remove : ");
         int removeVar = int.parse(stdin.readLineSync()!);
         myArray.remove(removeVar);
         print(myArray);
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         break;
       case 4:
         stdout.write("You are Viewing the List : ");
         myArray.forEach((element) {
           stdout.write("$element  ");
         });
+        print("");
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         break;
       case 0:
-        print("Exiting...");
+        print("Exiting the Menu Driven Code...");
         break;
       default:
         print("Invalid Choice!!");
