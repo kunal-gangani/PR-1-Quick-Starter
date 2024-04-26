d42f3665af373551ffb7c9af297b265a9b3a6c34
Kunal Gangani <150250846+kunal-gangani@users.noreply.github.com>
2024-04-26 15:30:46 +0530

    Update Que1.dart

--- Que1.dart
+++ Que1.dart
@@ -1,22 +1,23 @@
 import 'dart:io';
-void main(){
-  List myArray = [];
+
+void main() {
+  List<int> myArray = [];
   stdout.write("How many elements do you want in your Array : ");
   int len = int.parse(stdin.readLineSync()!);
   myArray = List.generate(len, (i) {
-      stdout.write("Enter the value for Element ${i+1} : ");
-      int el = int.parse(stdin.readLineSync()!);
-      myArray.add(el);
-      return el;
-    });
+    stdout.write("Enter the value for Element ${i+1} : ");
+    int el = int.parse(stdin.readLineSync()!);
+    myArray.insert(i, el); 
+    return el;
+  });
   print("Elements in the Array are : ");
   myArray.forEach((element) {
-      stdout.write("$element   ");
+    stdout.write("$element   ");
   });
   print("");
   print("Negative Elements in the Array are : ");
   myArray.forEach((element) {
-    if(element<0){
+    if (element < 0) {
       stdout.write("$element   ");
     }
   });
