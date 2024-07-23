eef7e61b61d552d66cb6f79f0234249660b4c88d
Kunal Gangani <150250846+kunal-gangani@users.noreply.github.com>
2024-07-23 13:12:20 +0530

    Update Que1.dart

--- Que1.dart
+++ Que1.dart
@@ -1,20 +1,24 @@
 import 'dart:io';
 
 void main() {
-    List<int> myArray = [];
-    stdout.write("How many elements do you want in your Array : ");
-    int len = int.parse(stdin.readLineSync()!);
-    myArray = List.generate(len, (i) {
-      stdout.write("Enter the value for Element ${i+1} : ");
-      int el = int.parse(stdin.readLineSync()!);
-      myArray.insert(i,el);
-      return el;
-    });
-    int large = myArray[0];
-    myArray.forEach((element) {
-      if(element>large){
-        large = element;
-      }
-    });
-    print("The Largest Element in the Array is : $large");
+  List<int> myArray = [];
+  stdout.write("How many elements do you want in your Array : ");
+  int len = int.parse(stdin.readLineSync()!);
+  myArray = List.generate(len, (i) {
+    stdout.write("Enter the value for Element ${i+1} : ");
+    int el = int.parse(stdin.readLineSync()!);
+    myArray.insert(i, el); 
+    return el;
+  });
+  print("Elements in the Array are : ");
+  myArray.forEach((element) {
+    stdout.write("$element   ");
+  });
+  print("");
+  print("Negative Elements in the Array are : ");
+  myArray.forEach((element) {
+    if (element < 0) {
+      stdout.write("$element   ");
+    }
+  });
 }
